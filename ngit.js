5a045ef3eb203c1ae1cc72fcc2ccdd338a166921
Jason Cox <JasonWilburCox@gmail.com>
2013-11-03 13:44:07 -0500

    Adding backlog of tasks for the git flow script

--- ngit.js
+++ ngit.js
@@ -2,6 +2,51 @@
 
 console.log('GIT!');
 
+/*
+Inprogress:
+
+Prioritized:
+
+Backlog:
+
+Setup npm for project
+Pick and intall cli parser
+Pick and install colorer
+Setup cli parsing
+Checkout branches from origin into local repos
+
+Setting my branches
+  - Current
+  - Other
+  - Release
+  - Hotfix
+  - 1-9
+  - Unsetting
+  - Max and Javon
+
+
+List of branches
+  - gitlist
+  - colors
+  - Numberify branches to be able to set branch by number
+  - Interactive mode that lets you select/unselect branches for different modes
+
+General branch management
+  - Switching
+  - Merging
+  - Updating
+  - Updating and merging
+  - Merging a list of branches
+  - Updating, then merging a list of branches
+  - Merging in a list of branches
+  - Merging in a list of branches to a list of branches
+
+DB Migrations
+  - Running migrations after swiching to a branch
+  - Getting a list of migrations no in another branch
+  - Rolling back the list of migrations not in a branch
+ */
+
 // What to do here?
 //  - Keep track of many branches, not just current and other
 //  - Update, merge, switch
