fff84ee85af99ea226882de189ee0dacddae0a1b
Jason Cox <JasonWilburCox@gmail.com>
2013-11-03 13:10:03 -0500

    Initial commit for my git flow script

---
+++ ngit.js
@@ -0,0 +1,10 @@
+#!/usr/local/bin/node
+
+console.log('GIT!');
+
+// What to do here?
+//  - Keep track of many branches, not just current and other
+//  - Update, merge, switch
+//  - Merge into a list of branches
+//  - Rollback branches before switching to another branch
+//  - Run migrations after swiching to a branch
