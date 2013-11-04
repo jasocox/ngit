2fccd3e7a1354f361beb68155a274e259eccd105
Jason Cox <JasonWilburCox@gmail.com>
2013-11-03 22:36:27 -0500

    Removing project desirables

--- ngit.js
+++ ngit.js
@@ -140,10 +140,3 @@ Setup npm for project
 VERSIONing
 
  */
-
-// What to do here?
-//  - Keep track of many branches, not just current and other
-//  - Update, merge, switch
-//  - Merge into a list of branches
-//  - Rollback branches before switching to another branch
-//  - Run migrations after swiching to a branch
